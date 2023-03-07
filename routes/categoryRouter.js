8f2e503d63bd94b2d2299797f46f8797260a799b
MezentsevaPolina <85152891+MezentsevaPolina@users.noreply.github.com>
2023-03-07 15:47:33 +0700

    Delete routes directory

--- routes/categoryRouter.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express')
-const router = new Router()
-const categoryController = require('../controllers/categoryController')
-const checkRole = require('../middleware/chekRoleMiddleware')
-
-router.post('/',checkRole('ADMIN'), categoryController.create)
-router.get('/',categoryController.getAll)
-
-module.exports = router
